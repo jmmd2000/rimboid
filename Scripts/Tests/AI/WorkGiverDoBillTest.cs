993ccc62d5a176191b6f14169e18a64f99d50bfd
James Doyle <63932603+jmmd2000@users.noreply.github.com>
2026-06-28 23:46:10 +0100

    test: cover bill stop-conditions and the do-bill work-giver

---
+++ Scripts/Tests/AI/WorkGiverDoBillTest.cs
@@ -0,0 +1,69 @@
+using Godot;
+using GdUnit4;
+using static GdUnit4.Assertions;
+
+[TestSuite]
+[RequireGodotRuntime]
+public class WorkGiverDoBillTest
+{
+    [BeforeTest]
+    public void Setup()
+    {
+        ItemDefOf.Load();
+        RecipeDefOf.Load();
+        BuildingDefOf.Load();
+        TerrainDefOf.Load();
+
+        Game.Map = new GameMap(10, 10);
+        for (int x = 0; x < Game.Map.Width; x++)
+            for (int y = 0; y < Game.Map.Height; y++)
+                Game.Map.Terrain[x, y] = TerrainDefOf.Dirt;
+
+        Game.Pathing = new Pathing();
+        Game.Pathing.Init(Game.Map);
+    }
+
+    static Building Stove(Vector2I cell)
+    {
+        var stove = Game.Map.SpawnBuilding(BuildingDefOf.Stove, cell);
+        foreach (var c in stove.OccupiedCells) Game.Pathing.RefreshCell(Game.Map, c);
+        return stove;
+    }
+
+    [TestCase]
+    public void OffersBillWhenIngredientsAvailable()
+    {
+        var stove = Stove(new Vector2I(5, 5));
+        stove.WorkBench.Bills.Add(new Bill { Recipe = RecipeDefOf.CookSimpleMeal, RepeatMode = BillRepeatMode.DoForever });
+        Game.Map.SpawnItem(ItemDefOf.Wheat, new Vector2I(2, 2), 4);
+        var guy = new Guy { Position = Vector2.Zero };
+
+        var job = new WorkGiver_DoBill().TryGiveJob(guy);
+
+        AssertObject(job).IsNotNull();
+        AssertBool(job.Type == JobType.DoBill).IsTrue();
+        AssertBool(job.TargetCell == new Vector2I(5, 5)).IsTrue();
+    }
+
+    [TestCase]
+    public void NoJobWhenIngredientsMissing()
+    {
+        var stove = Stove(new Vector2I(5, 5));
+        stove.WorkBench.Bills.Add(new Bill { Recipe = RecipeDefOf.CookSimpleMeal, RepeatMode = BillRepeatMode.DoForever });
+        var guy = new Guy { Position = Vector2.Zero };
+
+        AssertObject(new WorkGiver_DoBill().TryGiveJob(guy)).IsNull();
+    }
+
+    [TestCase]
+    public void NoJobWhenBillSatisfied()
+    {
+        var stove = Stove(new Vector2I(5, 5));
+        stove.WorkBench.Bills.Add(new Bill { Recipe = RecipeDefOf.CookSimpleMeal, RepeatMode = BillRepeatMode.UntilYouHave, TargetCount = 1 });
+        Game.Map.SpawnItem(ItemDefOf.SimpleMeal, new Vector2I(2, 2), 1);
+        Game.Map.SpawnItem(ItemDefOf.Wheat, new Vector2I(3, 3), 4);
+        var guy = new Guy { Position = Vector2.Zero };
+
+        AssertObject(new WorkGiver_DoBill().TryGiveJob(guy)).IsNull();
+    }
+}
---
+++ Scripts/Tests/Model/BillTest.cs
@@ -0,0 +1,38 @@
+using Godot;
+using GdUnit4;
+using static GdUnit4.Assertions;
+
+[TestSuite]
+[RequireGodotRuntime]
+public class BillTest
+{
+    [BeforeTest]
+    public void Setup()
+    {
+        ItemDefOf.Load();
+        RecipeDefOf.Load();
+        Game.Map = new GameMap(10, 10);
+    }
+
+    [TestCase]
+    public void DoForeverAlwaysWantsMore()
+    {
+        var bill = new Bill { Recipe = RecipeDefOf.CookSimpleMeal, RepeatMode = BillRepeatMode.DoForever };
+        AssertBool(bill.ShouldDo).IsTrue();
+    }
+
+    [TestCase]
+    public void UntilYouHaveWantsMoreBelowTarget()
+    {
+        var bill = new Bill { Recipe = RecipeDefOf.CookSimpleMeal, RepeatMode = BillRepeatMode.UntilYouHave, TargetCount = 10 };
+        AssertBool(bill.ShouldDo).IsTrue();   // 0 stored < 10
+    }
+
+    [TestCase]
+    public void UntilYouHaveStopsAtTarget()
+    {
+        Game.Map.SpawnItem(ItemDefOf.SimpleMeal, new Vector2I(2, 2), 10);
+        var bill = new Bill { Recipe = RecipeDefOf.CookSimpleMeal, RepeatMode = BillRepeatMode.UntilYouHave, TargetCount = 10 };
+        AssertBool(bill.ShouldDo).IsFalse();  // 10 stored >= 10
+    }
+}
