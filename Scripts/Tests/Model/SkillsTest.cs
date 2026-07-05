20e95bb32c01d2c28070431a37bb7ff1f71e50c9
James Doyle <63932603+jmmd2000@users.noreply.github.com>
2026-07-05 22:55:23 +0100

    test: add skills tests

---
+++ Scripts/Tests/Model/SkillsTest.cs
@@ -0,0 +1,64 @@
+using GdUnit4;
+using static GdUnit4.Assertions;
+
+[TestSuite]
+[RequireGodotRuntime]
+public class SkillsTest
+{
+    static readonly SkillDef Def = new() { DefName = "TestSkill", Label = "test skill" };
+
+    [TestCase]
+    public void StartsAtLevelZeroWithNoXP()
+    {
+        var skill = new Skills().Get(Def);
+        AssertInt(skill.Level).IsEqual(0);
+        AssertFloat(skill.XP).IsEqual(0f);
+    }
+
+    [TestCase]
+    public void XPBelowTheCurveDoesNotLevel()
+    {
+        var skills = new Skills();
+        skills.Gain(Def, 50f);
+        var skill = skills.Get(Def);
+        AssertInt(skill.Level).IsEqual(0);
+        AssertFloat(skill.XP).IsEqual(50f);
+    }
+
+    [TestCase]
+    public void LevellingUpCarriesTheRemainder()
+    {
+        var skills = new Skills();
+        skills.Gain(Def, 120f);
+        var skill = skills.Get(Def);
+        AssertInt(skill.Level).IsEqual(1);
+        AssertFloat(skill.XP).IsEqual(20f);
+    }
+
+    [TestCase]
+    public void OneLumpCanClimbSeveralLevels()
+    {
+        var skills = new Skills();
+        skills.Gain(Def, 350f);
+        var skill = skills.Get(Def);
+        AssertInt(skill.Level).IsEqual(2);
+        AssertFloat(skill.XP).IsEqual(50f);
+    }
+
+    [TestCase]
+    public void LevelIsCappedAtMaxLevel()
+    {
+        var skills = new Skills();
+        skills.Gain(Def, 1_000_000f);
+        AssertInt(skills.Get(Def).Level).IsEqual(Skills.MaxLevel);
+    }
+
+    [TestCase]
+    public void GainWithNullSkillIsANoOp()
+    {
+        var skills = new Skills();
+        skills.Gain(null, 100f);
+        skills.Gain(Def, 50f);
+        AssertFloat(skills.Get(Def).XP).IsEqual(50f);
+    }
+}
